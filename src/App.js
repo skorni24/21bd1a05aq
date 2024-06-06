d2cc9bf8d0ce9e399fefe52abb9e4a4ac2a2b97c
SrinivasKorni <21bd1a05aqcsef@gmail.com>
2024-06-06 18:51:49 +0530

    empty commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import ProductDetails from './components/ProductDetails';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path="/" element={<ProductList/>} />
-      <Route path="/product/:id" element={<ProductDetails/>} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route exact path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
